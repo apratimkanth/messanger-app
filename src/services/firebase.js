bdc8bc7e8a4d83cfb0b0f9c4ec7a833219602220
apratimkanth <apratimkanth.official@gmail.com>
2023-10-21 16:56:39 +0530

    first commit

---
+++ src/services/firebase.js
@@ -0,0 +1,78 @@
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    serverTimestamp,
+    onSnapshot,
+    query,
+    orderBy,
+} from 'firebase/firestore';
+
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfIDwRAk-TGmxRcYwbGIuBTP99hgdfLvM",
+  authDomain: "messanger-app-214cc.firebaseapp.com",
+  projectId: "messanger-app-214cc",
+  storageBucket: "messanger-app-214cc.appspot.com",
+  messagingSenderId: "545108646438",
+  appId: "1:545108646438:web:14bbe7843ec2f41ccc24e7"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function loginWithGoogle() {
+    try {
+        const provider = new GoogleAuthProvider();
+        const auth = getAuth();
+
+        const { user } = await signInWithPopup(auth, provider);
+
+        return { uid: user.uid, displayName: user.displayName };
+    } catch (error) {
+        if (error.code !== 'auth/cancelled-popup-request') {
+            console.error(error);
+        }
+        return null;
+    }
+}
+
+async function sendMessage(roomId, user, text) {
+    try {
+        await addDoc(collection(db, 'chat-rooms', roomId, 'messages'), {
+            uid: user.uid,
+            displayName: user.displayName,
+            text: text.trim(),
+            timestamp: serverTimestamp(),
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function getMessages(roomId, callback) {
+    return onSnapshot(
+        query(
+            collection(db, 'chat-rooms', roomId, 'messages'),
+            orderBy('timestamp', 'asc')
+        ),
+        (querySnapshot) => {
+            const messages = querySnapshot.docs.map((x) => ({
+                id: x.id,
+                ...x.data(),
+            }));
+
+            callback(messages);
+        }
+    );
+}
+
+export { loginWithGoogle, sendMessage, getMessages };
